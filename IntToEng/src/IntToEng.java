c3fb0bcbf371a0499323762437265c0ccaedee51
tatatamura <g15950tk@gm.tsuda.ac.jp>
2018-05-17 14:17:22 +0900

    100以下の位を表現しようとした（できてない）

--- IntToEng/src/IntToEng.java
+++ IntToEng/src/IntToEng.java
@@ -12,33 +12,55 @@ public class IntToEng {
 	
     static String translateEng(int n) {
     	String[] Eng = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-    	String[] Eng10 = {"ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+    	//String[] Engt = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+    	//String[] Eng10 = {"ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
     	String[] Eng100 = {"hundred","thousand"};
     	
     	StringBuffer sb =new StringBuffer("");
     	
-    	if(n==0){
-    		sb.append(Eng[n]);
-    		return sb.toString();
+    	//分岐：200,300,400,500,600,700,800,900
+    	if(n>100 && 1000>n){
+    		//100の位の数
+    		int y = n/100;
+    		translateEb(y);
+    		//hundredを出す
+    		int g = n%100;
+    		sb.append(Eng100[g]);
+    		//10の位以下の数
+    		translateEb(g);
     	}
-    	
-    	if(n!=0&&n<10){
-    		sb.append(Eng[n]);
+      	if(n==100){
+    		//100を変換
+    		sb.append(Eng100[0]);
+    	}else if(n==1000){
+    		//1000変換
+    		sb.append(Eng100[1]);
     	}
-    	if(n>=10&&n<100){
-    		int s = n/10;
-    		sb.append(Eng10[s]);
-    		int m = n%10;
-    		sb.append(Eng[m]);
-    		return sb.toString();
-    	}
-    	
-    	
-    	sb.append(Eng[n]);
-    	
-    
     	
         return sb.toString();
+    }
+    static String translateEb(int m) {
+    	String[] Eng = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+    	String[] Engt = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+    	String[] Eng10 = {"ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+    	StringBuffer sb =new StringBuffer("");
+    	
+    	if(m<10){
+    		//0~9を変換
+    		sb.append(Eng[m]);
+    	}else if(m>10&&m<20){
+    		sb.append(Engt[m-11]);
+    	}else if(m>=10&&m<100){
+    		//20~99を変換
+    		//10の位の数
+    		int s = m/10;
+    		sb.append(Eng10[s]);
+    		//1の位の数
+    		int t = m%10;
+    		sb.append(Eng[t]);
+    	}
+    	
+    	return sb.toString();
     }
 
 }
