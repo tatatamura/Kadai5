b4ab49d9a5269b848d6f4c9d4e949ef7f886f7d9
satokombj <satokombj@gmail.com>
2018-05-19 06:13:29 +0900

    0~999まで表示できるようにしました

--- IntToEng/src/IntToEng.java
+++ IntToEng/src/IntToEng.java
@@ -11,56 +11,41 @@ public class IntToEng {
 	}
 	
     static String translateEng(int n) {
-    	String[] Eng = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-    	//String[] Engt = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-    	//String[] Eng10 = {"ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+    	String[] Eng = {"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+    	String[] Eng10 = {"twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
     	String[] Eng100 = {"hundred","thousand"};
     	
     	StringBuffer sb =new StringBuffer("");
-    	
-    	//分岐：200,300,400,500,600,700,800,900
-    	if(n>100 && 1000>n){
-    		//100の位の数
-    		int y = n/100;
-    		translateEb(y);
-    		//hundredを出す
-    		int g = n%100;
-    		sb.append(Eng100[g]);
-    		//10の位以下の数
-    		translateEb(g);
+  	
+    	if(n/100==1) { 
+    		//100~199
+        	sb.append(Eng100[0]);   
+       		n = n%100; //上一桁を消す
+       		if (n==0) { //100
+       			return sb.toString();
+       		}
+   		}
+    	else if(n/100>1) {
+    		//200~999
+   			int x = n/100; //100の位の数
+   			sb.append(Eng[x]);
+   			sb.append(Eng100[0]);
+   			n = n%100; //上一桁を消す
+   			//System.out.println(n);//test
     	}
-      	if(n==100){
-    		//100を変換
-    		sb.append(Eng100[0]);
-    	}else if(n==1000){
-    		//1000変換
-    		sb.append(Eng100[1]);
+    	if(n<20){ 
+    		//1~19 
+        	sb.append(Eng[n]);
     	}
-    	
-        return sb.toString();
-    }
-    static String translateEb(int m) {
-    	String[] Eng = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-    	String[] Engt = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-    	String[] Eng10 = {"ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
-    	StringBuffer sb =new StringBuffer("");
-    	
-    	if(m<10){
-    		//0~9を変換
-    		sb.append(Eng[m]);
-    	}else if(m>10&&m<20){
-    		sb.append(Engt[m-11]);
-    	}else if(m>=10&&m<100){
-    		//20~99を変換
-    		//10の位の数
-    		int s = m/10;
-    		sb.append(Eng10[s]);
-    		//1の位の数
-    		int t = m%10;
-    		sb.append(Eng[t]);
-    	}
-    	
+   		else if(n/10>0) {
+   			//20~99
+    		int y = n/10;
+    		sb.append(Eng10[y-2]);
+   			n = n%10;
+   			if(n!=0) { //1の位が0の場合
+   				sb.append(Eng[n%10]);
+   			}
+   		}
     	return sb.toString();
     }
-
 }
